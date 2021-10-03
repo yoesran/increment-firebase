53b5d7d934dd75a191df650ccb0799de8151d08f
yoesran <19523170naufal@gmail.com>
2021-10-03 21:24:04 +0800

    first

---
+++ Assets/Scripts/GameOverController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            // reload
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            UserDataManager.Progress.Gold = 0;
+            UserDataManager.Progress.ResourcesLevels.Clear();
+            UserDataManager.Save();
+        }
+    }
+}
